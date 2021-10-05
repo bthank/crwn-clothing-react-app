62d0ae11120caceab608fdf37ffc24ec94dabdda
bthank <bthankachan@yahoo.com>
2021-10-05 12:03:26 -0400

    basic navigation changes

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 // initially create this component as a functional component since it doesn't need
 // to maintain any state
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div 
-        className={`${ size } menu-item`}>
+        className={`${ size } menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{
             backgroundImage: `url(${ imageUrl })`
         }} />
@@ -16,4 +17,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>    
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
